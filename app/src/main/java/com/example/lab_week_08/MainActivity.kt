00a071c2ea38c5262d391570c070cb34ef1c3ce6
waerooo <muhammadabidzarp69@gmail.com>
2025-11-05 13:20:06 +0700

    Commit No. 1 – add first & second worker

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.lab_week_08
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.work.*
+import com.example.lab_week_08.worker.FirstWorker
+import com.example.lab_week_08.worker.SecondWorker
 
 class MainActivity : AppCompatActivity() {
+
+    private val workManager = WorkManager.getInstance(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,50 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val networkConstraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val id = "001"
+
+        val firstRequest = OneTimeWorkRequest
+            .Builder(FirstWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(FirstWorker.INPUT_DATA_ID, id))
+            .build()
+
+        val secondRequest = OneTimeWorkRequest
+            .Builder(SecondWorker::class.java)
+            .setConstraints(networkConstraints)
+            .setInputData(getIdInputData(SecondWorker.INPUT_DATA_ID, id))
+            .build()
+
+        workManager.beginWith(firstRequest)
+            .then(secondRequest)
+            .enqueue()
+
+        workManager.getWorkInfoByIdLiveData(firstRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("First process is done")
+                }
+            }
+
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this) { info ->
+                if (info.state.isFinished) {
+                    showResult("Second process is done")
+                }
+            }
+    }
+
+    private fun getIdInputData(idKey: String, idValue: String) =
+        Data.Builder()
+            .putString(idKey, idValue)
+            .build()
+
+    private fun showResult(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/example/lab_week_08/worker/SecondWorker.kt
@@ -0,0 +1,26 @@
+package com.example.lab_week_08.worker
+
+import android.content.Context
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class SecondWorker(
+    context: Context,
+    workerParams: WorkerParameters
+) : Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+        val id = inputData.getString(INPUT_DATA_ID)
+        Thread.sleep(3000L)
+        val outputData = Data.Builder()
+            .putString(OUTPUT_DATA_ID, id)
+            .build()
+        return Result.success(outputData)
+    }
+
+    companion object {
+        const val INPUT_DATA_ID = "inId"
+        const val OUTPUT_DATA_ID = "outId"
+    }
+}
